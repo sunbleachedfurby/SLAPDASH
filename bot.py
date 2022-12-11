248ab4e963787d0343414dda1c4259eb5be53fe9
g <ginadevincenzi@GMD-MacBook-Air.local>
2022-12-11 12:19:01 -0500

    get twitter working

--- bot.py
+++ bot.py
@@ -1,29 +1,35 @@
 import requests 
 import json 
+import tweepy
 from dotenv import dotenv_values
 
 secrets = dotenv_values(".env")
 
-wordurl = "https://wordsapiv1.p.rapidapi.com/words/" 
+# wordurl = "https://wordsapiv1.p.rapidapi.com/words/" 
 
-headers = { "X-RapidAPI-Key" : secrets["rapidapi_key"],
-"X-RapidAPI-Host" : "wordsapiv1.p.rapidapi.com" 
-} 
-querystring = { "random" : "true" } 
+# headers = { "X-RapidAPI-Key" : secrets["rapidapi_key"],
+# "X-RapidAPI-Host" : "wordsapiv1.p.rapidapi.com" 
+# } 
+# querystring = { "random" : "true" } 
 
-r = requests.get ( wordurl, headers = headers, params = querystring ) 
-worddata = json.loads ( r.text )
-print ( worddata [ "word" ] )
+# r = requests.get ( wordurl, headers = headers, params = querystring ) 
+# worddata = json.loads ( r.text )
+# print ( worddata [ "word" ] )
 
-randomword = ( worddata [ "word" ] )
+# randomword = ( worddata [ "word" ] )
 
-metsearchurl = f"https://collectionapi.metmuseum.org/public/collection/v1/search?q={randomword}"
-print(metsearchurl)
-r = requests.get ( metsearchurl )
-metdata = json.loads ( r.text )
-metobjectids = ( metdata [ "objectIDs" ] )
-print(metobjectids)
+# metsearchurl = f"https://collectionapi.metmuseum.org/public/collection/v1/search?q={randomword}"
+# print(metsearchurl)
+# r = requests.get ( metsearchurl )
+# metdata = json.loads ( r.text )
+# metobjectids = ( metdata [ "objectIDs" ] )
+# print(metobjectids)
 
-metobjecturl = f"https://collectionapi.metmuseum.org/public/collection/v1/objects/{metobjectids[0]}"
-r = requests.get ( metobjecturl )
-print ( r.text )
+# metobjecturl = f"https://collectionapi.metmuseum.org/public/collection/v1/objects/{metobjectids[0]}"
+# r = requests.get ( metobjecturl )
+# print ( r.json()["primaryImage"] )
+
+client = tweepy.Client(consumer_key=secrets["consumer_key"], consumer_secret=secrets["consumer_secret"], access_token=secrets["access_token"], access_token_secret=secrets["access_token_secret"]
+)
+resp=client.create_tweet(text="first tweet")
+print(resp)
